bc0630a9adc874d5ab9fb837587a1f17d48f6b58
Edvinas Adomaitis <t4skmanag3r@gmail.com>
2025-05-24 14:46:46 +0300

    init, basic setup for tauri + vuejs + tailwindcss

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main class="container">
+  </main>
+</template>
+
+<style scoped>
+</style>
+<style>
+
+body {
+  background-color: #252525;
+}
+
+h1, h2, h3, h4, h5, h6 {
+  font-family: monospace
+}
+p {
+  font-family: sans-serif
+}
+
+</style>
